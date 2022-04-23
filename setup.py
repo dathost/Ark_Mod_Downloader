4bcd8923d6687059eeea4c247aa181bf46d65491
Svante Boberg <svante.boberg@gmail.com>
2022-04-23 09:38:47 +0200

    Pin steamfiles version

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from distutils.core import setup
 setup(
     name="ark_mod_downloader",
     packages=["ark_mod_downloader"],
-    install_requires=["steamfiles @ git+https://github.com/dathost/steamfiles.git"],
+    install_requires=[
+        "steamfiles @ git+https://github.com/dathost/steamfiles.git@af354648fa9c47747ea1e34f1e9ce07e1be7e330"
+    ],
 )
